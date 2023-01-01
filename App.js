7dc7b9a6e430826a2d6444ac6c66322dd883bdbf
stephen-97 <stephen.loiola@hotmail.com>
2023-01-01 17:18:47 +0100

    main

--- App.js
+++ App.js
@@ -1,19 +1,23 @@
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View } from 'react-native';
 import BottomBarNav from './src/navigation/BottomBarNav';
+import {store} from "./src/redux/redux";
+import { Provider } from 'react-redux';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <BottomBarNav />
-    </View>
-  );
-}
+      <Provider store={store}>
+        <View style={styles.container}>
+          <BottomBarNav />
+        </View>
+      </Provider>
+    );
+  }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+  const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor: '#fff',
+      alignItems: 'center',
+      justifyContent: 'center',
+    },
+  });
